c04475740e7a3bfdeb6697014bc940c305cb75c5
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2024-09-19 10:35:47 +0200

    Templats + bootstrap + heritage

--- GestionProduits/Produits/urls.py
+++ GestionProduits/Produits/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.products, name="products"),
     path('<int:product_id>', views.product, name="product"),
+    path('<int:product_id>/update', views.update, name="product_update"),
+    path('<int:product_id>/delete', views.delete, name="product_update"),
+    path('add', views.add, name="product_add"),
 ]
--- GestionProduits/Produits/views.py
+++ GestionProduits/Produits/views.py
@@ -3,18 +3,36 @@ from .models import Product
 
 # Create your views here.
 def products(request):
-    products = Product.objects.all()
-    print(products)
-    return render(
-        request,
-        'products.html',
-        {'products': products}
-    )
+  products = Product.objects.all()
+  print(products)
+  return render(
+    request,
+    'products.html',
+    {'products': products}
+  )
 
 def product(request, product_id):
-    product = Product.objects.get(id=product_id)
-    return render(
-        request,
-        'product.html',
-        {'product': product}
-    )
+  product = Product.objects.get(id=product_id)
+  return render(
+    request,
+    'product.html',
+    {'product': product}
+  )
+
+def add(request):
+  return render(
+    request,
+    'product_add.html'
+  )
+
+def update(request, product_id):
+  product = Product.objects.get(id=product_id)
+  return render(
+    request,
+    'product_update.html',
+    {'product': product}
+  )
+
+def delete(request, product_id):
+  product = Product.objects.get(id=product_id)
+  product.delete()
